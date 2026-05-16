7825da5e0a33eb6da39fe09b380763687d27218e
Devashish Gogoi <hellonat3456@gmail.com>
2026-05-16 15:38:06 +0530

    Create _worker.js

---
+++ _worker.js
@@ -0,0 +1,10 @@
+import { createRequestHandler } from "@remix-run/cloudflare";
+import * as build from "./build/server/index.js";
+
+const handleRequest = createRequestHandler(build);
+
+export default {
+  async fetch(request, env, ctx) {
+    return handleRequest(request, { env, ctx });
+  },
+};
