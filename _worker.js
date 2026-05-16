6d43ab1cd3df8502d42e8000cc5886fb87222e7c
Devashish Gogoi <hellonat3456@gmail.com>
2026-05-16 16:00:03 +0530

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,6 +5,13 @@ const handleRequest = createRequestHandler(build);
 
 export default {
   async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    
+    // Let Cloudflare Pages serve static assets directly
+    if (url.pathname.startsWith("/assets/") || url.pathname.startsWith("/images/")) {
+      return env.ASSETS.fetch(request);
+    }
+
     return handleRequest(request, { env, ctx });
   },
 };
